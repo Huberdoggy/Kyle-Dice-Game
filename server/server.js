499780048ba9bb8b5dc60afeed6c17e4c37e060f
Kyle Huber <huberdoggy@gmail.com>
2020-06-08 20:19:11 -0500

    server spinning up on 5000, mvd static files to public due to improper rendering

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
-//app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Serve back static files by default
 app.use(express.static('server/public'));
